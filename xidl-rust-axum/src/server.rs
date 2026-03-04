a3bdcbc4765394b5c962df46a8b4564972361d43
loongtao.zhang <loongtao.zhang@outlook.com>
2026-03-04 12:56:12 +0800

    feat(rust-axum): add serve_with_listener
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- xidl-rust-axum/src/server.rs
+++ xidl-rust-axum/src/server.rs
@@ -1,21 +1,10 @@
 use super::*;
 
-pub struct Io {
-    listener: tokio::net::TcpListener,
-}
-
-impl Io {
-    pub fn new(listener: tokio::net::TcpListener) -> Self {
-        Self { listener }
-    }
-}
-
 pub trait Service: Send + Sync + 'static {
     fn into_router(self) -> axum::Router;
 }
 
 pub struct ServerBuilder {
-    io: Option<Io>,
     router: axum::Router,
 }
 
@@ -24,30 +13,32 @@ pub struct Server;
 impl Server {
     pub fn builder() -> ServerBuilder {
         ServerBuilder {
-            io: None,
             router: axum::Router::new(),
         }
     }
 }
 
 impl ServerBuilder {
-    pub fn with_io(mut self, io: Io) -> Self {
-        self.io = Some(io);
-        self
-    }
-
     pub fn with_service<T: Service>(mut self, svc: T) -> Self {
         self.router = self.router.merge(svc.into_router());
         self
     }
 
     pub async fn serve(self, addr: impl tokio::net::ToSocketAddrs) -> Result<()> {
-        let listener = match self.io {
-            Some(io) => io.listener,
-            None => tokio::net::TcpListener::bind(addr)
-                .await
-                .map_err(|err| Error::new(500, err.to_string()))?,
-        };
+        let listener = tokio::net::TcpListener::bind(addr)
+            .await
+            .map_err(|err| Error::new(500, err.to_string()))?;
+        axum::serve(listener, self.router)
+            .await
+            .map_err(|err| Error::new(500, err.to_string()))
+    }
+
+    pub async fn serve_with_listener<L>(self, listener: L) -> Result<()>
+    where
+        L: axum::serve::Listener,
+        axum::serve::Serve<L, axum::Router, axum::Router>:
+            std::future::IntoFuture<Output = std::io::Result<()>>,
+    {
         axum::serve(listener, self.router)
             .await
             .map_err(|err| Error::new(500, err.to_string()))
