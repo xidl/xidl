5752502295bc21d55884477629900a13ca6e0cfb
loongtao.zhang <loongtao.zhang@outlook.com>
2026-03-07 13:38:56 +0800

    test(xidlc-examples): add more example
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- xidlc-examples/build.rs
+++ xidlc-examples/build.rs
@@ -1,23 +1,29 @@
-use std::path::Path;
-
 use xidl_build::Builder;
 
 fn main() {
-    let axum_idl = Path::new("examples/hello_world.idl");
-    let jsonrpc_idl = Path::new("examples/hello_world_jsonrpc.idl");
-
     println!("cargo:rerun-if-changed=../xidlc/**/*.j2");
-    println!("cargo:rerun-if-changed={}", axum_idl.display());
-    println!("cargo:rerun-if-changed={}", jsonrpc_idl.display());
+    println!("cargo:rerun-if-changed=./examples");
     println!("cargo:rerun-if-changed=build.rs");
 
     Builder::new()
         .with_lang("rust-axum")
-        .compile(&[axum_idl])
+        .compile(&["./api/hello_world.idl"])
+        .expect("failed to compile idl for rust-axum example");
+
+    Builder::new()
+        .with_lang("openapi")
+        .with_out_dir("./api/")
+        .compile(&["./api/hello_world.idl"])
         .expect("failed to compile idl for rust-axum example");
 
     Builder::new()
         .with_lang("rust-jsonrpc")
-        .compile(&[jsonrpc_idl])
+        .compile(&["./api/hello_world_jsonrpc.idl"])
+        .expect("failed to compile idl for rust-jsonrpc example");
+
+    Builder::new()
+        .with_lang("openrpc")
+        .with_out_dir("./api")
+        .compile(&["./api/hello_world_jsonrpc.idl"])
         .expect("failed to compile idl for rust-jsonrpc example");
 }
