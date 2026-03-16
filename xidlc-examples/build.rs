973eadc4abe4bce5cbf90b01be3d98f0037412fc
loongtao.zhang <loongtao.zhang@outlook.com>
2026-03-16 19:00:47 +0800

    refactor(xidl-examples): refactor build.rs
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- xidlc-examples/build.rs
+++ xidlc-examples/build.rs
@@ -1,65 +1,53 @@
 use xidl_build::Builder;
 
+fn build_openapi(file_name: &str) {
+    Builder::new()
+        .with_lang("rust-axum")
+        .compile(&[format!("./api/http/{file_name}.idl")])
+        .expect("failed to compile city http idl for rust-axum example");
+    Builder::new()
+        .with_lang("openapi")
+        .with_out_dir("./api/http/generated/")
+        .with_output_filename(format!("{file_name}.json"))
+        .compile(&[format!("./api/http/{file_name}.idl")])
+        .expect("failed to compile city http stream openapi");
+}
+
+fn build_jsonrpc(file_name: &str) {
+    Builder::new()
+        .with_lang("rust-jsonrpc")
+        .compile(&[format!("./api/jsonrpc/{file_name}.idl")])
+        .expect("failed to compile city jsonrpc idl for rust-jsonrpc example");
+    Builder::new()
+        .with_lang("openrpc")
+        .with_out_dir("./api/jsonrpc/generated/")
+        .with_output_filename(format!("{file_name}_openrpc.json"))
+        .compile(&[format!("./api/jsonrpc/{file_name}.idl")])
+        .expect("failed to compile city jsonrpc stream openrpc");
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=../xidlc/**/*.j2");
     println!("cargo:rerun-if-changed=./examples");
     println!("cargo:rerun-if-changed=build.rs");
 
-    Builder::new()
-        .with_lang("rust-axum")
-        .compile(&["./api/hello_world.idl"])
-        .expect("failed to compile idl for rust-axum example");
+    for file in glob::glob("./api/http/*.idl").expect("failed to read glob pattern") {
+        let path = file.expect("failed to read path from glob");
+        let file_name = path
+            .file_stem()
+            .expect("failed to get file stem")
+            .to_str()
+            .expect("failed to convert file stem to string");
+        build_openapi(file_name);
+    }
 
-    Builder::new()
-        .with_lang("rust-axum")
-        .compile(&["./api/city_http.idl"])
-        .expect("failed to compile city http idl for rust-axum example");
-
-    Builder::new()
-        .with_lang("rust-axum")
-        .compile(&["./api/city_http_stream.idl"])
-        .expect("failed to compile city http stream idl for rust-axum example");
-
-    Builder::new()
-        .with_lang("openapi")
-        .with_out_dir("./api/")
-        .with_output_filename("city_openapi.json")
-        .compile(&["./api/city_http.idl"])
-        .expect("failed to compile idl for rust-axum example");
-
-    Builder::new()
-        .with_lang("openapi")
-        .with_out_dir("./api/")
-        .with_output_filename("city_http_stream_openapi.json")
-        .compile(&["./api/city_http_stream.idl"])
-        .expect("failed to compile city http stream openapi");
-
-    Builder::new()
-        .with_lang("rust-jsonrpc")
-        .compile(&["./api/hello_world_jsonrpc.idl"])
-        .expect("failed to compile idl for rust-jsonrpc example");
-
-    Builder::new()
-        .with_lang("rust-jsonrpc")
-        .compile(&["./api/city_jsonrpc.idl"])
-        .expect("failed to compile city jsonrpc idl for rust-jsonrpc example");
-
-    Builder::new()
-        .with_lang("rust-jsonrpc")
-        .compile(&["./api/city_jsonrpc_stream.idl"])
-        .expect("failed to compile city jsonrpc stream idl for rust-jsonrpc example");
-
-    Builder::new()
-        .with_lang("openrpc")
-        .with_out_dir("./api")
-        .with_output_filename("city_openrpc.json")
-        .compile(&["./api/city_jsonrpc.idl"])
-        .expect("failed to compile idl for rust-jsonrpc example");
-
-    Builder::new()
-        .with_lang("openrpc")
-        .with_out_dir("./api")
-        .with_output_filename("city_jsonrpc_stream_openrpc.json")
-        .compile(&["./api/city_jsonrpc_stream.idl"])
-        .expect("failed to compile city jsonrpc stream openrpc");
+    for file in glob::glob("./api/jsonrpc/*.idl").expect("failed to read glob pattern") {
+        let path = file.expect("failed to read path from glob");
+        let file_name = path
+            .file_stem()
+            .expect("failed to get file stem")
+            .to_str()
+            .expect("failed to convert file stem to string");
+        build_jsonrpc(file_name);
+    }
 }
