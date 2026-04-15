24d996f872d26203069a8a11be56b22d1f5c90f5
loongtao.zhang <loongtao.zhang@outlook.com>
2026-04-15 16:51:39 +0800

    feat(xidlc): impl default for rust struct
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- xidlc-examples/tests/hello_world.rs
+++ xidlc-examples/tests/hello_world.rs
@@ -4,4 +4,9 @@ use xidlc_examples::hello_world::*;
 fn test_basic() {
     assert_eq!(EnumFirstDefault::default(), EnumFirstDefault::First);
     assert_eq!(EnumSecondDefault::default(), EnumSecondDefault::Second);
+
+    let _ = SimpleStruct::default();
+    let c = ComplexStruct::default();
+    assert_eq!(c.c, EnumFirstDefault::First);
+    assert_eq!(c.d, EnumSecondDefault::Second);
 }
